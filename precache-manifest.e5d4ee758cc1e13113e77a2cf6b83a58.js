d3f49d5a4b912faf95abc504e341b055faded3c9
Kevin Oliveira <kevin.zero@hotmail.com>
2021-07-03 22:45:39 -0300

    Updates

---
+++ precache-manifest.e5d4ee758cc1e13113e77a2cf6b83a58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cdcde44add7c9d384d2d7091da9bd2c",
+    "url": "/project-movie-cards-library/index.html"
+  },
+  {
+    "revision": "53167de4ec0caa74062a",
+    "url": "/project-movie-cards-library/static/css/main.7deb8bba.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/project-movie-cards-library/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "53167de4ec0caa74062a",
+    "url": "/project-movie-cards-library/static/js/main.29eec935.chunk.js"
+  },
+  {
+    "revision": "b5cd559347f125a80e22",
+    "url": "/project-movie-cards-library/static/js/runtime-main.136c6182.js"
+  }
+]);
